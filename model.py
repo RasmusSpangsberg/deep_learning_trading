9b9f4be3982d5495fadef2b20da967141b3cb72b
RasmusSpangsberg <rasmus.spangsberg@hotmail.com>
2022-12-06 22:01:36 +0100

    network doesn't learn, but no errors

--- model.py
+++ model.py
@@ -1,12 +1,32 @@
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
 import torch
 
 
-class Model:
-	def __init__(self, env):
-		self.env = env
+class PolicyNet(nn.Module):
+    def __init__(self, env):
+        super(PolicyNet, self).__init__()
+        self.env = env
+        # network
+        self.hidden = nn.Linear(40, 60)
+        self.out = nn.Linear(60, 1)
 
-	def predict(self, observation):
-		return self.env.action_space.sample()
+        # training
+        self.optimizer = optim.Adam(self.parameters(), lr=1)
 
-	def fit(self, reward):
-		pass
+    def forward(self, observation):
+        x = torch.Tensor(observation).view(40)
+        x = self.hidden(x)
+        x = F.relu(x)
+        x = self.out(x)
+        #x = F.sigmoid(x)
+        x = F.softmax(x)
+        print("x after softmax:", x)
+        x = torch.round(x)
+        print("x after round:", x)
+
+        return x
+
+    def loss(self, action_probabilities, reward):
+        return -torch.mean(torch.mul(torch.log(action_probabilities), reward))
--- validation.py
+++ validation.py
@@ -1,30 +1,42 @@
 import gym
 from gym_anytrading.datasets import FOREX_EURUSD_1H_ASK
 import matplotlib.pyplot as plt
-from model import Model
+from model import PolicyNet
 
 
 env = gym.make('forex-v0',
                df = FOREX_EURUSD_1H_ASK,
-               window_size = 10,
-               frame_bound = (10, 300),
+               window_size = 20,
+               frame_bound = (20, 300),
                unit_side = 'right')
 
-model = Model(env)
+policy = PolicyNet(env)
 
+losses = []
+training_rewards = []
 
 observation = env.reset()
 while True:
-    action = model.predict(observation)
+    action = policy(observation)
     observation, reward, done, info = env.step(action)
 
-    model.fit(reward)
+    # policy gradient update
+    policy.optimizer.zero_grad()
+    a_probs = policy(observation)
+    loss = policy.loss(a_probs, reward)
+    loss.backward()
+    policy.optimizer.step()
+
+    training_rewards.append(reward)
+    losses.append(loss.item())
 
     if done:
         print("info:", info)
         break
 
 print("max possible profit:", env.max_possible_profit())
+print("losses:", losses)
+print("training rewards:", training_rewards)
 plt.cla()
 env.render_all()
 plt.show()
